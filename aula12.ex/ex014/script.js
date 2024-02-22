2221881a79c185d045d2ebcb3d88599c2df2c968
kaique_kletelinger <149007831+Kaique091@users.noreply.github.com>
2024-02-22 18:48:48 -0300

    23

--- aula12.ex/ex014/script.js
+++ aula12.ex/ex014/script.js
@@ -1,1 +1,10 @@
-alert("oi")
+function carregar(){
+var msg = document.getElementById('msg')
+var img = document.getElementById('imagem')
+var data = new Date()
+var hora = data.getHours()
+msg.innerHTML = `agora sao ${hora} horas`
+
+  if ( hora >= 0 && hora < 18 )
+  img.scr = 'dia.png'
+} 
